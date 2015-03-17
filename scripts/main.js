5bd2272459201dfc981fd1c37ac0be82440a67ad
hewx <hewx@mail.yht.com>
2015-03-17 14:01:46 +0800

    change options

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,16 @@ require(["opentact"], function(opentact) {
     var sdk = opentact.SDK.getInstance();
     var rest = opentact.Rest.getInstance();
 
-    var options = {sid: '',
-        ssid: '',
-        authToken: '',
+    var options = {sid: '54ddd9e6afd12966c2ea9589',
+        ssid: '54e05227afd12909af009c5e',
+        authToken: '88e77509bd0e4dc89b8e915a8db25ebe',
         remoteElement: 'remoteVideo',
         localElement: 'localVideo',
         onRegistered: function(){},
         onDisconnected: function(){},
-        onIncoming: function() {
-            return true;
-    }};
+        onIncoming: function() {}};
 
-    sdk.setOptions();
+    sdk.setOptions(options);
 
     sdk.startUpVoice(function() {
         // OnRegistered callback
@@ -27,9 +25,14 @@ require(["opentact"], function(opentact) {
     });
 
     // make a call to sid
-    sdk.makeCallToSid("1234564ssrerer455");
+    sdk.makeCallToSid("54e05211afd12909af009c5c");
 
     // send message to sid
-    sdk.sendMessage({"message": "hello"}, "1234564ssrerer455");
+    sdk.sendMessage({
+        message: "hello",
+        timestamp: new Date(),
+        type: "text/plain",
+        fsid: options.ssid
+    }, "54e05211afd12909af009c5c");
 
 });
