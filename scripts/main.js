8b326196b58c97b79bba43e63f7942d0df95fe0b
hewx <hewx@mail.yht.com>
2015-04-28 17:29:21 +0800

    change sipjs to sipml

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,20 @@ require(["opentact"], function(opentact) {
     var sdk = opentact.SDK.getInstance();
     var rest = opentact.Rest.getInstance();
 
-    var options = {sid: '54ddd9e6afd12966c2ea9589',
-        ssid: '54e05227afd12909af009c5e',
-        authToken: '88e77509bd0e4dc89b8e915a8db25ebe',
-        remoteElement: 'remoteVideo',
-        localElement: 'localVideo',
-        onRegistered: function(){},
-        onDisconnected: function(){},
-        onIncoming: function() {}};
+    var options = {sid: '55288821afd1293a0540a3d4',
+        ssid: '552888a9afd129390300f9fc',
+        authToken: '875f27edf68d4d79adf1e803a891ae2e',
+        audioRemoteElement: 'audio-remote',
+        videoRemoteElement: 'video-remote',
+        videoLocalElement: 'video-local'};
 
     sdk.setOptions(options);
 
     sdk.startUpVoice(function() {
         // OnRegistered callback
+
+        // now make a call to sid
+        sdk.makeCallToSid("552888afafd129390300f9fe");
     }, function() {
         // OnDisconnected
     }, function(sid) {
@@ -24,8 +25,9 @@ require(["opentact"], function(opentact) {
         sdk.answerCall();
     });
 
-    // make a call to sid
-    sdk.makeCallToSid("54e05211afd12909af009c5c");
+    sdk.startIM(function() {
+
+    });
 
     // send message to sid
     sdk.sendMessage({
--- scripts/opentact.js
+++ scripts/opentact.js
@@ -1,9 +1,9 @@
 
-define(['jquery', 'sip', 'mqtt'], function($, sip, mqtt) {
+define(['jquery', 'SIPml-api', 'mqtt'], function($, sip, mqtt) {
     var version = "v1";
     var host = "192.168.1.28";
-    var ws_servers = 'ws://192.168.1.28:10080';
-    var sip_server = "192.168.1.28";
+    var ws_server = 'ws://192.168.1.28:10060';
+    var sip_server = "192.168.1.6:6060";
     var im_server = "192.168.1.28";
 
     var Rest = (function(){
@@ -79,64 +79,109 @@ define(['jquery', 'sip', 'mqtt'], function($, sip, mqtt) {
         var instance;
 
         function init() {
-            var ua;
+            var sipStack;
             var session;
-            var options;
+
             var onRegistered;
             var onDisconnected;
             var onIncoming;
 
+
+            var login = function() {
+                registerSession = sipStack.newSession('register', {
+                    events_listener: {events: '*', listener: eventsListener}
+                });
+                registerSession.register();
+            };
+
+            var acceptCall = function(e) {
+                session = e.newSession;
+                var remoteUri = session.getRemoteFriendlyName();
+                var rest = Rest.getInstance();
+                rest.sipAccountByNumber(remoteUri, function(data) {
+                    onIncoming(data.sid)
+                });
+                // e.newSession.accept();
+                // e.newSession.reject() to reject the call
+            };
+
+            function eventsListener(e) {
+                console.info("Status: " + e.type);
+
+                if (e.type == 'started') {
+                    // ready for registering
+                    login();
+                } else if(e.type == 'i_new_call') {
+                    // incoming audio/video call
+                    acceptCall(e);
+                } else if (e.type == 'connected' && e.session == registerSession) {
+                    // registered
+                    //makeCall();
+                    onRegistered();
+                }  else if (e.type == 'stopped') {
+                    // stopped
+                    onDisconnected();
+                }
+            }
+
             return {
-                register: function(username, password, remoteElement, localElement, onRegisteredCallback, onDisconnectedCallback, onIncomingCallback) {
-                    var config = {
-                        traceSip: true,
-                        uri: username + '@'+ sip_server,
-                        ws_servers: ws_servers,
-                        authorizationUser: username,
-                        password: password,
-                        register: true
-                    };
-                    options = {
-                        media: {
-                            constraints: {
-                                audio: true,
-                                video: false
-                            },
-                            render: {
-                                remote: {
-                                    video: document.getElementById(remoteElement)
-                                },
-                                local: {
-                                    video: document.getElementById(localElement)
-                                }
+                register: function(username, password, audioRemoteElement, videoRemoteElement, videoLocalElement, onRegisteredCallback, onDisconnectedCallback, onIncomingCallback) {
+
+                    SIPml.init(function(e) {
+                        sipStack = new SIPml.Stack({
+                                realm: sip_server, // mandatory: domain name
+                                impi: username, // mandatory: authorization name (IMS Private Identity)
+                                impu: 'sip:' + username + '@' + sip_server, // mandatory: valid SIP Uri (IMS Public Identity)
+                                password: password, // optional
+                                display_name: username, // optional
+                                websocket_proxy_url: ws_server, // optional
+                                outbound_proxy_url: 'udp://' + sip_server, // optional
+                                enable_rtcweb_breaker: true, // optional
+                                events_listener: { events: '*', listener: eventsListener }, // optional: '*' means all events
+                                sip_headers: [ // optional
+                                        { name: 'User-Agent', value: 'web-client opentact v0.1' },
+                                        { name: 'Organization', value: 'Opentact' }
+                                ]
                             }
-                        }
-                    };
+                        );
+
+                        sipStack.start();
+                    }, function(e){
+                        console.error('Failed to initialize the engine: ' + e.message);
+                    });
 
                     onRegistered = onRegisteredCallback;
                     onDisconnected = onDisconnectedCallback;
                     onIncoming = onIncomingCallback;
 
-                    ua = new sip.UA(config);
-                    ua.start();
+                    audioRemote = audioRemoteElement;
+                    videoRemote = videoRemoteElement;
+                    videoLocal = videoLocalElement;
 
-                    ua.on('invite', function(ss) {
-                        session = ss;
-                        var from = new RegExp("\"(.*?)\" <.*?>;tag=.*?");
-                        var matches = from.exec(session.request.from);
-                        var ssid = matches[1];
 
-                        onIncoming(ssid);
-                    });
                 },
                 invite:  function(uri) {
-                    session = this.ua.invite(uri, options);
+                    session = sipStack.newSession('call-audio', {
+                        video_local: document.getElementById(videoLocal),
+                        video_remote: document.getElementById(videoRemote),
+                        audio_remote: document.getElementById(audioRemote),
+                        events_listener: { events: '*', listener: eventsListener },
+                        sip_caps: [
+                            { name: '+g.oma.sip-im' },
+                            { name: 'language', value: '\"en,fr\"' }
+                        ]
+                    });
+
+                    session.call(uri);
                 },
                 hangup: function() {
-                    session.bye();
+                    session.hangup();
                 },
                 accept: function() {
                     session.accept()
+                },
+                reject: function() {
+                    session.reject();
                 }
             };
         }
@@ -217,13 +262,9 @@ define(['jquery', 'sip', 'mqtt'], function($, sip, mqtt) {
                         sid: '',
                         ssid: '',
                         authToken: '',
-                        remoteElement: 'remoteVideo',
-                        localElement: 'localVideo',
-                        onRegistered: function(){},
-                        onDisconnected: function(){},
-                        onIncoming: function() {
-                            return true;
-                        }
+                        audioRemoteElement: 'audio_remote',
+                        videoRemoteElement: 'video_remote',
+                        videoLocalElement: 'video_local'
                     };
 
                     options = $.extend({}, defaults, _options || {});
@@ -235,14 +276,16 @@ define(['jquery', 'sip', 'mqtt'], function($, sip, mqtt) {
                 startUpVoice: function(onRegisteredCallback, onDisconnectedCallback, onIncomingCallback) {
                     var rest = Rest.getInstance();
                     rest.sipAccountBySid(options.ssid, function(data) {
-                        ua.register(data.sip_number, data.sip_password, options.remoteElement, options.localElement,
+                        ua.register(data.sip_number, data.sip_password, options.audioRemoteElement,
+                                    options.videoRemoteElement, options.videoLocalElement,
                                     onRegisteredCallback, onDisconnectedCallback, onIncomingCallback);
 
                     });
+
                 },
                 startIM: function(incomingCallback) {
                     im = IM.getInstance();
-                    im.register(im_server, options.ssid, options.ssid, options.authToken, "opetact/" + options.sid + "/" + options.ssid,
+                    im.register(im_server, options.ssid, options.ssid, options.authToken, "opentact/" + options.sid + "/" + options.ssid,
                                 incomingCallback);
                 },
                 sendMessage: function(message, sid) {
@@ -252,14 +295,14 @@ define(['jquery', 'sip', 'mqtt'], function($, sip, mqtt) {
                 makeCallToSid:function(sid) {
                     var rest = Rest.getInstance();
                     rest.sipAccountBySid(sid, function(data) {
-                        ua.invite("sip:11111@" + data.sip_number + "@" + sip_server);
+                        ua.invite("sip:11111" + data.sip_number + "@" + sip_server);
                     });
                 },
                 makeCallToTermination:function(number)
                 {
                     var rest = Rest.getInstance();
                     rest.sipAccountBySid(sid, function(data) {
-                        ua.invite("sip:99999@" + number + "@" + sip_server);
+                        ua.invite("sip:99999" + number + "@" + sip_server);
                     });
                 },
                 endCall: function() {
